51d9d8551982122805c8b86ccad5f87d432c6b5e
Christian Bromann <git@bromann.dev>
2022-11-28 10:48:27 -0800

    setup firefox

--- test/wdio.conf.ts
+++ test/wdio.conf.ts
@@ -1,6 +1,17 @@
+import { exec } from 'node:child_process'
+import { promisify } from 'node:util'
 import type { Options, Services } from '@wdio/types'
 
 import GeckoDriverLauncher from '../dist/index.js'
+
+/**
+ * set firefox path in CI
+ * see https://github.com/browser-actions/setup-firefox/issues/359
+ */
+const FIREFOX_BINARY_PATH = await promisify(exec)('which firefox').then(
+    ({ stdout }) => stdout,
+    () => undefined
+)
 
 export const config: Options.Testrunner = {
     automationProtocol: 'webdriver',
@@ -9,7 +20,8 @@ export const config: Options.Testrunner = {
         maxInstances: 5,
         browserName: 'firefox',
         'moz:firefoxOptions': {
-            args: ['-headless']
+            args: ['-headless'],
+            ...(FIREFOX_BINARY_PATH ? { binary: FIREFOX_BINARY_PATH } : {})
         }
     }],
     logLevel: 'trace',
